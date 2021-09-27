2b71df1d5228933fbe665e7512be69e31bd72d88
Jiang Wenjian <jwenjian.me@gmail.com>
2021-09-27 18:23:08 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var scrolled = false;
+var canScroll = false;
+
+function bodyScroll(e) {
+  console.log(el)
+  scrolled = true
+  if (!canScroll) {
+    alert('dead')
+  }
+}
+
+window.onload = function(e) {
+  
+}
